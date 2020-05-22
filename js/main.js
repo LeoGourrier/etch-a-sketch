e60aa8337f97f89d6afeee3f32d51ed6a6b5239a
LG <leogourrier@gmail.com>
2020-05-22 12:37:09 +0200

    Added Reset Button

--- js/main.js
+++ js/main.js
@@ -1,7 +1,34 @@
 let numOfSquares = 27;
 
+function insertResetButton() {
+	let buttonContainer = document.createElement('div');
+	buttonContainer.id = 'buttonContainer';
+	let button = document.createElement('button');
+	button.textContent = 'Reset';
+	button.id = 'resetBtn';
+	buttonContainer.appendChild(button);
+	let body = document.querySelector('body');
+	body.appendChild(buttonContainer);
+	body.insertBefore(buttonContainer, body.firstChild);
+}
+function addHoverToReset() {
+	let button = document.querySelector('#resetBtn');
+	button.addEventListener('click', function() {
+		//get all squares
+		let squares = Array.from(document.querySelectorAll('.squares'));
+		//remove yellow class from squares
+		squares.map((x) => (x.classList = _removeClass(x.classList.value, 'yellow')));
+		//prompt the user for number of squares
+		//add that number to numOfSquares
+		numOfSquares = prompt('How many Squares?', 16);
+		insertSquares(numOfSquares);
+		addHoverToSquares();
+		//alert(numOfSquares);
+	});
+}
 function insertSquares(num) {
 	let sketchBoard = document.querySelector('#etch');
+	sketchBoard.innerHTML = '';
 	sketchBoard.style = `grid-template-columns: repeat(${num},1fr);
 	grid-template-rows: repeat(${num},1fr);`;
 
@@ -15,16 +42,39 @@ function insertSquares(num) {
 		}
 	}
 }
-
 function addHoverToSquares() {
 	let listOfSquares = Array.from(document.querySelectorAll('.squares'));
 	listOfSquares.map((x) => {
 		x.addEventListener('mouseenter', function() {
-			x.classList += ' yellow';
-			console.log(x.classList);
+			x.classList = _addClass(x.classList.value, 'yellow');
+			//console.log(x.classList);
 		});
 	});
 }
+function _addClass(str, a) {
+	//take class string
+	//convert it to array
+	str = str.split(' ');
+	//add class to array
+	str.push(a);
+	//convert array to string with spaces
+	str = str.join(' ');
+	//return this string
+	return str;
+}
+function _removeClass(str, a) {
+	//take class string
+	//convert it to array
+	str = str.split(' ');
+	//remove class from array if it's in there
+	str = str.filter((x) => x != a);
+	//convert array to string with spaces
+	str = str.join(' ');
+	//return this string
+	return str;
+}
 
+insertResetButton();
+addHoverToReset();
 insertSquares(numOfSquares);
 addHoverToSquares();
